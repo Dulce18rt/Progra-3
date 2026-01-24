36c367ac55be8c2cf08f366d095b9cbecb93b0de
[Dulce18rt] <[dreyesp11@miumg.edu.gt]>
2026-01-23 18:40:21 -0600

    Mi primer Programa

---
+++ primerproy3/src/primerproy3/Primerproy3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primerproy3;
+
+/**
+ *
+ * @author Jorge Reyes
+ */
+public class Primerproy3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Hola Mundo");
+    }
+    
+}
